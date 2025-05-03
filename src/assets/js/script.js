098b56055632af92c5d5a7990ec686f178787dd9
doctorscript <kelsondesousa99@gmail.com>
2025-05-03 23:38:49 +0100

    Correcção do js

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -3,5 +3,5 @@ const menu = document.querySelector('.mobile-menu')
 
 show_menu.addEventListener('click', () => {
     menu.classList.toggle('hidden')
-    show_menu.src = menu.classList.contains('hidden') ? './src/assets/images/menu-images/menu_white_36dp.svg' : './src/assets/images/menu-images/close_white_36dp.svg'
+    show_menu.src = menu.classList.contains('hidden') ? 'src/assets/images/menu-images/menu_white_36dp.svg' : 'src/assets/images/menu-images/close_white_36dp.svg'
 })
